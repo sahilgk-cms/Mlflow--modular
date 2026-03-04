afff7d9aa3531493c98cfbb7c986317dbd70aff3
sahilgk-cms <155707816+sahilgk-cms@users.noreply.github.com>
2026-03-04 17:48:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,8 @@ def main():
 
         tscv = TimeSeriesSplit(n_splits=N_CV_SPLITS)
 
-        metric_fn = MetricFactory.get_metric(MODEL_NAME)
+        optimization_metric = MetricFactory.get_optimize_metric(MODEL_NAME)
+        eval_metrics = MetricFactory.get_eval_metric(MODEL_NAME)
 
         param_space_function = SearchSpaceFactory.get_search_space(
             model_name=MODEL_NAME,
