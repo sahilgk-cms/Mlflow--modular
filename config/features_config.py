9bbf1db3185515c05fc015bd4c80b94e8b16678e
sahilgk-cms <sahil@catalysts.org>
2026-03-16 11:52:32 +0530

    config files

---
+++ config/features_config.py
@@ -0,0 +1,8 @@
+
+
+STATE = "Andhra Pradesh"
+DIAGNOSIS = "Scrub Typhus"
+
+OUTPUT_TABLE = "ap_final_1"
+OUTPUT_SCHEMA = "dev_gold"
+
---
+++ config/filepaths.py
@@ -0,0 +1,8 @@
+import os
+
+CURRENT_DIR = os.getcwd()
+
+
+SEARCH_SPACE_PATH = os.path.join(CURRENT_DIR, "config", "search_spaces.yml")
+
+
