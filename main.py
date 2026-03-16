53772f7098e1142108218f47c89653a406e827c3
sahilgk-cms <sahil@catalysts.org>
2026-03-16 14:24:15 +0530

    main

--- main.py
+++ main.py
@@ -1,51 +1,37 @@
-from data.data_loader import load_raw_data, sort_data, get_data_hash, extract_data_metadata, temporal_train_test_split, split_features_target, drop_null_values, save_dataframe_to_csv
+from pipelines.features_builder import build_features
+from pipelines.data_builder import build_data
+from db.engine import get_engine
+from datetime import datetime
 from preprocessing.factory import PreprocessorFactory
-from utils.mlflow_helpers import start_mlflow_experiment, register_model_with_data_tags
+from utils.mlflow_helpers import start_mlflow_experiment
+from utils.artifact_logger import log_parquet
+from utils.mlflow_helpers import register_model_with_data_tags
+from utils.explainability import log_shap_summary
+from training.trainer import TimeSeriesTrainer
 from pipelines.train_pipeline import run_training_pipeline
 from pipelines.evaluation_pipeline import run_evaluation_pipeline
-from training.search_space_factory import SearchSpaceFactory
-from training.eval_factory import MetricFactory
-from training.trainer import TimeSeriesTrainer
-from training.optimizer import OptunaOptimizer
-from training.cv_factory import CVFactory
-from models.factory import ModelFactory
-from datetime import datetime
 import mlflow
-from utils.explainability import log_shap_summary
-from utils.artifact_logger import log_parquet
-from sklearn.model_selection import TimeSeriesSplit
-from config import *
+from config.env import DB_NAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_USER
+from config.features_config import STATE, DIAGNOSIS, OUTPUT_SCHEMA, OUTPUT_TABLE
+from config.data_config import DISEASE
+from config.ml_config import USE_GPU, PREDICTIONS_PATH, SHAP_SUMMARY_PATH, OPTIMIZER_TYPE, SHAP_VALUES_PATH, EVALUATION_RUN_TYPE, FEATURE_IMPORTANCE_PATH, TRAINING_RUN_TYPE, PREPROCESSOR_NAME, MLFLOW_URI, EXPERIMENT_NAME, TRAIN_PATH, TEST_PATH, CV_TYPE, MODEL_NAME, N_TRIALS, N_CV_SPLITS
 
 def main():
-    df = load_raw_data(path = FILEPATH, date_col = DATE_COL)
-
-    train_df, test_df = temporal_train_test_split(df, date_col=DATE_COL, cutoff_week=CUTOFF_WEEK)
-    train_df, test_df = sort_data(train_df, test_df, date_col=DATE_COL)
-
-    train_df, test_df = drop_null_values(train_df=train_df,
-                                         test_df=test_df,
-                                         target_col=TARGET_COL)
-
-
-
-    train_data_hash = get_data_hash(train_df)
-    test_data_hash = get_data_hash(test_df)
-
-    train_metadata = extract_data_metadata(train_df, date_col=DATE_COL, train=True)
-    test_metadata = extract_data_metadata(test_df, date_col=DATE_COL)
-
-    X_train, y_train = split_features_target(train_df, TARGET_COL, DATE_COL)
-    X_test, y_test, X_test_meta = split_features_target(test_df,
-                                                        TARGET_COL,
-                                                        DATE_COL,
-                                                        return_meta=True,
-                                                        test_meta_cols=TEST_META_COLS)
-
+    engine = get_engine(db_user=DB_USER, db_password=DB_PASSWORD, db_host=DB_HOST,
+                        db_port=DB_PORT, db_name=DB_NAME)
+    
+    # build_features(engine=engine, state=STATE, diagnosis=DIAGNOSIS, 
+    #                output_table=OUTPUT_TABLE, output_schema=OUTPUT_SCHEMA)
+    
+    output = build_data(engine=engine, disease=DISEASE)
+    X_train = output["features"]["X_train"]
+    y_train = output["features"]["y_train"]
+    X_test = output["features"]["X_test"]
+    y_test = output["features"]["y_test"]
 
     pre = PreprocessorFactory.create(PREPROCESSOR_NAME)
     pre.fit(X_train)
     X_train_preprocessed = pre.transform(X_train)
-
     X_test_preprocessed = pre.transform(X_test)
 
     feature_names = pre.get_feature_names()
@@ -60,75 +46,60 @@ def main():
         mlflow.set_tags(
             {
                 "preprocessor_name": PREPROCESSOR_NAME,
-                "train_data_hash": train_data_hash,
-                "test_data_hash": test_data_hash,
-                "train_date_min": train_metadata["train_date_min"],
-                "train_date_max": train_metadata["train_date_max"],
-                "test_date_min": test_metadata["test_date_min"],
-                "test_date_max": test_metadata["test_date_max"]
+                "train_data_hash": output["hash"]["train_data_hash"],
+                "test_data_hash": output["hash"]["test_data_hash"],
+                "train_date_min": output["metadata"]["train_metadata"]["train_date_min"],
+                "train_date_max": output["metadata"]["train_metadata"]["train_date_max"],
+                "test_date_min": output["metadata"]["test_metadata"]["test_date_min"],
+                "test_date_max": output["metadata"]["test_metadata"]["test_date_max"]
 
             }
         )
 
+        log_parquet(df = output["data"]["train_df"], filename=TRAIN_PATH, artifact_path="data")
+        log_parquet(df=output["data"]["test_df"], filename=TEST_PATH, artifact_path="data")
 
-        log_parquet(df = train_df, filename=TRAIN_PATH, artifact_path="data")
-        log_parquet(df=test_df, filename=TEST_PATH, artifact_path="data")
 
-        tscv = CVFactory.create(cv_type = CV_TYPE)
-
-        optimization_metric = MetricFactory.get_optimize_metric(MODEL_NAME)
-        eval_metrics = MetricFactory.get_eval_metrics(MODEL_NAME)
-
-        param_space_function = SearchSpaceFactory.get_search_space(
-            model_name=MODEL_NAME,
-            use_gpu=USE_GPU
-        )
-
-        model_cls, model_kwargs = ModelFactory.get_model(
-            model_name=MODEL_NAME,
-            cat_feature_indices=cat_feature_indices
-        )
-
-        final_model, best_cv_score, best_params, training_run_id = run_training_pipeline(X_train=X_train_preprocessed,
-                                                                  y_train=y_train,
-                                                                  param_space_fn=param_space_function,
-                                                                  model_cls=model_cls,
-                                                                  model_kwargs=model_kwargs,
-                                                                  cv=tscv,
-                                                                  trainer_cls=TimeSeriesTrainer,
-                                                                  optimizer_cls=OptunaOptimizer,
-                                                                  optimization_metric=optimization_metric,
-                                                                  model_name=MODEL_NAME,
-                                                                  run_type=TRAINING_RUN_TYPE,
-                                                                  cv_name=CV_TYPE,
-                                                                  pipeline_root_run_id=pipeline_root_run_id)
-
+        final_model, best_cv_score, best_params, training_run_id = run_training_pipeline(
+                                                                    X_train=X_train_preprocessed,
+                                                                    y_train=y_train,
+                                                                    model_name=MODEL_NAME,
+                                                                    cv_type=CV_TYPE,
+                                                                    optimizer_type=OPTIMIZER_TYPE,
+                                                                    trainer_cls=TimeSeriesTrainer,
+                                                                    use_gpu=USE_GPU,
+                                                                    n_trials=N_TRIALS,
+                                                                    n_cv_split=N_CV_SPLITS,
+                                                                    run_type=TRAINING_RUN_TYPE,
+                                                                    pipeline_root_run_id=pipeline_root_run_id,
+                                                                    cat_feature_indices=cat_feature_indices)
 
         if final_model.has_feature_importance():
             importance_df = final_model.get_feature_importance(feature_names=feature_names)
             importance_df = importance_df[importance_df["feature"] != "Case_Count"]
-            log_parquet(df=importance_df, filename=FEATURE_IMPORTANCE_PATH, artifact_path="feature_importance")
+            log_parquet(df=importance_df, filename=FEATURE_IMPORTANCE_PATH,
+                         artifact_path="feature_importance")
 
-
-        test_rmse = run_evaluation_pipeline(X_test=X_test_preprocessed,
+        metric_results = run_evaluation_pipeline(X_test=X_test_preprocessed,
                                 y_test = y_test,
-                                X_test_meta=X_test_meta,
+                                X_test_meta=output["test_meta"],
                                 model = final_model,
-                                eval_metric=eval_metric,
                                 best_cv_score=best_cv_score,
                                 predictions_path=PREDICTIONS_PATH,
                                 model_name=MODEL_NAME,
                                 run_type=EVALUATION_RUN_TYPE,
                                 pipeline_root_run_id=pipeline_root_run_id)
-
+        
         REGISTERED_MODEL_NAME = f"{EXPERIMENT_NAME}_{MODEL_NAME}"
         register_model_with_data_tags(
             training_run_id=training_run_id,
             model_name=REGISTERED_MODEL_NAME,
-            train_data_hash=train_data_hash,
-            test_data_hash=test_data_hash,
+            train_data_hash=output["hash"]["train_data_hash"],
+            test_data_hash=output["hash"]["test_data_hash"],
             pipeline_root_run_id=pipeline_root_run_id,
             preprocessor_name=PREPROCESSOR_NAME,
+            optimizer_type=OPTIMIZER_TYPE,
+            eval_metric_results=metric_results
         )
 
 
@@ -139,7 +110,3 @@ def main():
 
         log_parquet(df=shap_df, filename=SHAP_VALUES_PATH, artifact_path="explainability")
         mlflow.log_artifact(shap_summary_path, artifact_path="explainability")
-
-
-if __name__ == "__main__":
-    main()
